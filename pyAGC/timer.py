799bc3b26d3197360e40c38365defa595fde265c
Victor Douet <victor.douet@gmail.com>
2021-07-07 03:36:35 -0400

    Added license header to files

--- pyAGC/timer.py
+++ pyAGC/timer.py
@@ -1,3 +1,20 @@
+"""Copyright (C) 2021  Victor Douet
+
+This file is part of pyAGC.
+pyAGC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyAGC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyAGC.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 # from time import monotonic_ns as time_ns
 # from time import process_time_ns as time_ns
 # from time import time_ns as time_ns
